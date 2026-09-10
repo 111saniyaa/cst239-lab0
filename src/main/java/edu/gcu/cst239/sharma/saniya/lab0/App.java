db10ae52d8539de9f2da0c0b04835a23742e0443
111saniyaa <saniya.sharma.2061@gmail.com>
2026-09-09 21:28:10 -0700

    Add Lab 0 Hello World application

---
+++ src/main/java/edu/gcu/cst239/sharma/saniya/lab0/App.java
@@ -0,0 +1,13 @@
+package edu.gcu.cst239.sharma.saniya.lab0;
+
+
+    public class App {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Roster name: Saniya Sharma");
+        System.out.println("Hello, World!");
+    }
+}
+
+
